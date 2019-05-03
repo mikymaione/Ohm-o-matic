1302be8eb1cfe295ffa646274b7b5b645363f7d7
Michele Maione <mikymaione@hotmail.it>
2019-05-03 10:41:52 +0200

    server jersey

---
+++ src/main/java/OhmOMatic/REST/RestServer.java
@@ -0,0 +1,36 @@
+/*
+MIT License
+Copyright (c) 2019 Michele Maione
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package OhmOMatic.REST;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class RestServer
+{
+    public static final String BASE_URI = "http://localhost:8080/OOM/";
+
+    public static HttpServer startServer()
+    {
+        final ResourceConfig rc = new ResourceConfig().packages("OhmOMatic");
+
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        final HttpServer server = startServer();
+        System.out.println("Ohm-o-matic Server avviato!");
+        System.out.println(String.format("Visita %s", BASE_URI));
+        System.out.println("Premere invio per terminare...");
+        System.in.read();
+        server.stop();
+    }
+}
