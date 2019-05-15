eec5e5868e565a96ea2a1229928508157b511558
Michele Maione <mikymaione@hotmail.it>
2019-05-15 12:59:34 +0200

    buffer

--- src/main/java/OhmOMatic/Cli/CliCasa.java
+++ src/main/java/OhmOMatic/Cli/CliCasa.java
@@ -4,60 +4,40 @@ Copyright (c) 2019 Michele Maione
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-package OhmOMatic.Sistema;
+package OhmOMatic.Cli;
 
-import OhmOMatic.Simulation.SmartMeterSimulator;
-import OhmOMatic.Sistema.Base.BufferImpl;
+import OhmOMatic.Base.BaseCommandLineApplication;
+import OhmOMatic.Sistema.Casa;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
-public class Casa
+public final class CliCasa extends BaseCommandLineApplication
 {
 
-    private SmartMeterSimulator smartMeter;
-    private BufferImpl theBuffer = new BufferImpl();
-
-    
-    public Casa(String indirizzoServer, int numeroPorta, int ID)
+    public static void main(String[] args)
     {
-        smartMeter = new SmartMeterSimulator(theBuffer);
+        final Options options = CreaOpzioni();
+        StampaOpzioni("CliCasa", options);
+
+        try
+        {
+            CommandLine cmd = getCommandLine(options, args);
+
+            var casa = new Casa("www.stocazzo.it", 8888, 1);
+            casa.avviaSmartMeter();
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
     }
 
-
-    public void iscrivitiAlCondominio()
+    private static Options CreaOpzioni()
     {
+        var options = new Options();
 
+        return options;
     }
-
-    public void disiscrivitiDalCondominio()
-    {
-
-    }
-
-
-    public void calcolaConsumoEnergeticoComplessivo()
-    {
-
-    }
-
-    public void richiediAlCondominioDiPoterConsumareOltreLaMedia()
-    {
-
-    }
-
-    public void inviaStatisticheAlServer()
-    {
-
-    }
-
-
-    public void avviaSmartMeter()
-    {
-        smartMeter.run();
-    }
-
-    public void fermaSmartMeter()
-    {
-        smartMeter.stop();
-    }
-
 
 }
--- src/main/java/OhmOMatic/Sistema/Base/BufferImpl.java
+++ src/main/java/OhmOMatic/Sistema/Base/BufferImpl.java
@@ -4,60 +4,60 @@ Copyright (c) 2019 Michele Maione
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-package OhmOMatic.Sistema;
+package OhmOMatic.Sistema.Base;
 
-import OhmOMatic.Simulation.SmartMeterSimulator;
-import OhmOMatic.Sistema.Base.BufferImpl;
+import OhmOMatic.Simulation.Buffer;
+import OhmOMatic.Simulation.Measurement;
 
-public class Casa
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class BufferImpl implements Buffer
 {
 
-    private SmartMeterSimulator smartMeter;
-    private BufferImpl theBuffer = new BufferImpl();
+    public final int SlidingWindowCount;
 
-    
-    public Casa(String indirizzoServer, int numeroPorta, int ID)
+    public ArrayList<Double> Medie = new ArrayList<>();
+
+    private ArrayDeque<Measurement> buffer;
+
+
+    public BufferImpl()
     {
-        smartMeter = new SmartMeterSimulator(theBuffer);
+        this(24);
+    }
+
+    public BufferImpl(int SlidingWindowCount)
+    {
+        this.SlidingWindowCount = SlidingWindowCount;
+        buffer = new ArrayDeque<>(SlidingWindowCount);
     }
 
 
-    public void iscrivitiAlCondominio()
+    private void Elabora()
     {
+        var somma = 0.0;
 
+        for (var e : buffer)
+            somma += e.getValue();
+
+        var media = somma / SlidingWindowCount;
+        Medie.add(media);
+
+        for (var i = 0; i < SlidingWindowCount / 2; i++)
+            buffer.remove();
     }
 
-    public void disiscrivitiDalCondominio()
+    @Override
+    public void addMeasurement(Measurement m)
     {
+        synchronized (this)
+        {
+            buffer.add(m);
 
+            if (buffer.size() == SlidingWindowCount)
+                Elabora();
+        }
     }
-
-
-    public void calcolaConsumoEnergeticoComplessivo()
-    {
-
-    }
-
-    public void richiediAlCondominioDiPoterConsumareOltreLaMedia()
-    {
-
-    }
-
-    public void inviaStatisticheAlServer()
-    {
-
-    }
-
-
-    public void avviaSmartMeter()
-    {
-        smartMeter.run();
-    }
-
-    public void fermaSmartMeter()
-    {
-        smartMeter.stop();
-    }
-
 
 }
