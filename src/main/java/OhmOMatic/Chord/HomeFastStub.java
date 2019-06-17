fdd5aab72b907050b259caa6db8801712c2a10fa
Michele Maione <mikymaione@hotmail.it>
2019-06-17 18:19:07 +0200

    indentazione

--- src/main/java/OhmOMatic/Chord/HomeFastStub.java
+++ src/main/java/OhmOMatic/Chord/HomeFastStub.java
@@ -11,12 +11,24 @@ public class HomeFastStub implements AutoCloseable
 
 	private ManagedChannel chan;
 
-	public HomeServiceGrpc.HomeServiceBlockingStub getStub(InetSocketAddress destination)
+	public HomeServiceGrpc.HomeServiceBlockingStub getStub(InetSocketAddress destination) throws Exception
 	{
+		var ip = destination.getAddress().getHostAddress();
+		var port = destination.getPort();
+
 		chan = ManagedChannelBuilder
-				.forAddress(destination.getAddress().getHostAddress(), destination.getPort())
+				.forAddress(ip, port)
 				.usePlaintext()
 				.build();
+
+		var stato = chan.getState(true);
+
+		switch (stato)
+		{
+			case SHUTDOWN:
+			case TRANSIENT_FAILURE:
+				throw new Exception("Server " + ip + ":" + port + " is closed.");
+		}
 
 		return HomeServiceGrpc.newBlockingStub(chan);
 	}
