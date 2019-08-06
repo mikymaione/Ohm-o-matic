237a2db9cb11829f1119b09037ffc07da05ed4f6
Michele Maione <mikymaione@hotmail.it>
2019-08-06 13:28:56 +0200

    aggiustati bat

---
+++ CliCasa6.bat
@@ -0,0 +1,5 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.1
+set PATH=C:\Program Files\Java\jdk-11.0.1\bin\;%PATH%
+java -version
+java -cp target\Ohm-o-matic-jar-with-dependencies.jar OhmOMatic.Cli.CliCasa -r http://localhost:8080/OOM/OOM -i Kakashi -k 127.0.0.1 -q 9006 -j 127.0.0.1 -p 9001
